735b28969565de0b43e9aab5d83d9a7d9183376c
Your Name <your@email.com>
2026-06-14 15:52:52 +0530

    Setup Express server

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,1 +1,13 @@
-console.log("Survey Backend Started");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 10000;
+
+app.get("/", (req, res) => {
+    res.send("Survey Backend Running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
